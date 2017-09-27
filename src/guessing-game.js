1041107650efa75e9af74ee1712feaaf364d06ae
AlyaY <alya.yurevich@gmail.com>
2017-09-27 14:29:19 +0300

    added solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,29 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+      this.num = 0;
+    }
 
     setRange(min, max) {
-
+      this.min = min;
+      this.max = max;
+      this.num = Math.round((this.max - this.min)/2);
+      // return this;
     }
 
     guess() {
-
+      return this.num;
     }
 
     lower() {
-
+      this.max = this.num;
+      this.num = Math.round((this.max + this.min)/2);
+      // return this;
     }
 
     greater() {
-
+      this.min = this.num;
+      this.num = Math.round((this.max + this.min)/2);
+      // return this;
     }
 }
 
